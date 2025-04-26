d2ed93123f19e8a4729eceda7ec2c553584565c3
sanjaysanthosh140 <sanjaysanthosh140@gmail.com>
2025-04-26 21:22:56 +0530

    pattern06

---
+++ base_pattern/pattern06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int i,j,f,k,n=4,y;
+    for(i=-n;i<=n;i++){
+
+        if(i<0){
+            k= -i;
+        }else{
+            k = i;
+        }
+
+        for(j=0;j<k+1;j++){
+            printf(" ");
+        }
+
+        for(j=0,f=1;j<(n-k)*2+1;f++,j++){
+            printf("%d",f);
+        }
+        printf("\n");
+        // for(j=0,f=1;j<(n-k)*2+1;f++,j++){ 
+            // printf("%d",f); 
+            // } 
+            // printf("\n");
+
+    }
+}
